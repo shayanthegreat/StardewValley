97a44867a8eac013799663365650763622c67dca
Your Name <you@example.com>
2025-07-17 15:49:28 +0330

    added skills completed

---
+++ core/src/main/java/com/StardewValley/Models/Enums/AvatarType.java
@@ -0,0 +1,61 @@
+package com.StardewValley.Models.Enums;
+
+import com.StardewValley.Models.GameAssetManager;
+import com.StardewValley.Models.Map.Direction;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public enum AvatarType {
+    ABIGAIL("Abigail", GameAssetManager.getInstance().ABIGAIL,"female"),
+    SAM("Sam",GameAssetManager.getInstance().SAM,"male"),
+    Harvey("Harvey",GameAssetManager.getInstance().Harvey,"male");
+
+    private final String name;
+    private final Texture texture;
+    private final TextureRegion[][] textureRegion;
+    private final String gender;
+
+    AvatarType(String name, Texture texture, String gender) {
+        this.name = name;
+        this.texture = texture;
+        this.gender = gender;
+        this.textureRegion = TextureRegion.split(texture, 16, 32);
+    }
+
+    public Animation walkingAnimation(Direction direction) {
+        int x = 0;
+        switch (direction) {
+            case up ->{
+                x = 2;
+                break;
+            }
+            case down ->{
+                x = 0;
+                break;
+            }
+            case left, upLeft, downLeft->{
+                x = 3;
+                break;
+            }
+            case right, upRight,downRight->{
+                x = 1;
+                break;
+            }
+        }
+        Animation<TextureRegion> animation = new Animation(0.1f, textureRegion[x][1] , textureRegion[x][2] , textureRegion[x][3] , textureRegion[x][0] );
+        animation.setPlayMode(Animation.PlayMode.LOOP);
+        return animation;
+    }
+
+    public Animation TiredAnimation(Direction direction) {
+//        TODO : Return the tired animation
+        return null;
+    }
+
+    public Animation faintAnimation(Direction direction) {
+//        TODO
+        return null;
+    }
+
+}
--- core/src/main/java/com/StardewValley/Models/GameAssetManager.java
+++ core/src/main/java/com/StardewValley/Models/GameAssetManager.java
@@ -16,7 +16,7 @@ public class GameAssetManager {
         return gameAssetManager;
     }
     public final Texture CLOCK_ALL = new Texture("Clock/Clock_All.png");
-    public final Skin SKIN = new Skin(Gdx.files.internal("skin/craftacular-ui.json"));
+//    public final Skin SKIN = new Skin(Gdx.files.internal("skin/craftacular-ui.json"));
     public final BitmapFont MAIN_FONT = new BitmapFont();
     public final TextureRegion CLOCK_MAIN = new TextureRegion(CLOCK_ALL, 0, 0, 72, 59);
     public final TextureRegion CLOCK_ARROW = new TextureRegion(CLOCK_ALL, 72, 0, 8, 18);
@@ -2309,7 +2309,7 @@ public class GameAssetManager {
     public final Texture WILD_TREE_STUMP_FALL = new Texture("Trees/Wild_Tree_stump_Fall.png");
     public final Texture WILD_TREE_STUMP_SPRING = new Texture("Trees/Wild_Tree_stump_Spring.png");
     public final Texture WILD_TREE_STUMP_WINTER = new Texture("Trees/Wild_Tree_stump_Winter.png");
-    public final Texture ABIGAIL = new Texture("Villagers/Abigail.png");
+//    public final Texture ABIGAIL = new Texture("Villagers/Abigail.png");
     public final Texture ALEX = new Texture("Villagers/Alex.png");
     public final Texture BIRDIE = new Texture("Villagers/Birdie.png");
     public final Texture BOUNCER = new Texture("Villagers/Bouncer.png");
@@ -2348,7 +2348,7 @@ public class GameAssetManager {
     public final Texture PIERRE = new Texture("Villagers/Pierre.png");
     public final Texture PROFESSOR_SNAIL = new Texture("Villagers/Professor_Snail.png");
     public final Texture ROBIN = new Texture("Villagers/Robin.png");
-    public final Texture SAM = new Texture("Villagers/Sam.png");
+//    public final Texture SAM = new Texture("Villagers/Sam.png");
     public final Texture SANDY = new Texture("Villagers/Sandy.png");
     public final Texture SEBASTIAN = new Texture("Villagers/Sebastian.png");
     public final Texture SHANE = new Texture("Villagers/Shane.png");
@@ -2420,9 +2420,28 @@ public class GameAssetManager {
     public final Texture WOOD_CLUB = new Texture("Weapon/Wood_Club.png");
     public final Texture WOOD_MALLET = new Texture("Weapon/Wood_Mallet.png");
     public final Texture WOODEN_BLADE = new Texture("Weapon/Wooden_Blade.png");
+
+    // ALI
+
+    public Skin skin = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
+    public Skin SKIN = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
+
+    public Texture GRASS_FLOORING = new Texture("Stardew_Valley_Images-main/Stardew_Valley_Images-main/Flooring/Flooring_44.png");
+
+    public Texture STONE_FLOORING = new Texture("Stardew_Valley_Images-main/Stardew_Valley_Images-main/Flooring/Flooring_52.png");
+
+    public Texture ABIGAIL = new Texture("Stardew_Valley_Images-main/Stardew_Valley_Images-main/sprites/Abigail.png");
+
+    public Texture SAM = new Texture("Stardew_Valley_Images-main/Stardew_Valley_Images-main/sprites/Sam.png");
+
+    public Texture Harvey = new Texture("Stardew_Valley_Images-main/Stardew_Valley_Images-main/sprites/Harvey.png");
     {
         for (int i = 0; i < 12; i++) {
             ClOCK_MANNERS[i] = new TextureRegion(CLOCK_ALL, 80 + i % 4 * 13, i / 4 * 9, 13, 9);
         }
+    }
+
+    public Skin getSkin() {
+        return skin;
     }
 }
